0055b55169f707db3930f769a0c15d05b9c9e24e
Erik Inge Bolsø <knan-rosbif@anduin.net>
2009-09-20 00:44:03 +0200

    add hairy_method test for nginx version checking, fix false invalid http/0.9 detection on jetty 3.0

---
+++ urls.sh
@@ -0,0 +1,50 @@
+URLS=( 'http://localhost:8080/' \
+'http://localhost:631/' \
+'http://localhost:8081/' \
+'http://localhost:8082/' \
+'http://localhost:8083/' \
+'http://localhost:8084/' \
+'http://localhost:8085/' \
+'http://localhost:8086/' \
+'http://localhost:8087/' \
+'http://localhost:8088/' \
+'http://localhost:8089/' \
+'http://localhost:8090/' \
+'http://localhost:8091/' \
+'http://localhost:8092/' \
+'http://localhost:8093/' \
+'http://localhost:8094/' \
+'http://localhost:8095/' \
+'http://localhost:8096/' \
+'http://localhost:8097/' \
+'http://localhost:8098/' \
+'http://localhost:8099/' \
+'http://localhost:8100/' \
+'http://localhost:8101/' \
+) ;
+
+NAMES=( 'cern-httpd-3.0A' \
+'cups-1.3.9' \
+'jetty-1.3.5-demoapp' \
+'jetty-1.3.5-fileserver' \
+'jetty-2.4.9-demoapp' \
+'jetty-2.4.9-fileserver' \
+'boa-0.94.13' \
+'apache-1.2.6' \
+'apache-1.2.6-php3-loaded' \
+'apache-1.2.6-php3-used' \
+'boa-0.92r' \
+'boa-0.93.17.3' \
+'boa-0.94.14rc21' \
+'hydra-0.0.2' \
+'hydra-0.1.0' \
+'hydra-0.1.3' \
+'hydra-0.1.8' \
+'vlc-0.9.9-admin' \
+'nginx-0.5.38' \
+'nginx-0.6.39' \
+'nginx-0.7.62' \
+'nginx-0.8.15' \
+'jetty-3.0.6-demoapp' \
+) ;
+
