c6b9e18b22351edd0048ce0646e75c64e8078c94
Erik Inge Bolsø <knan-rosbif@anduin.net>
2009-09-23 23:30:22 +0200

    speedup

--- generate-sigs.sh
+++ generate-sigs.sh
@@ -2,15 +2,23 @@
 
 . urls.sh
 
-( for x in `seq 0 3 $((${#URLS[*]}-1))`; do
+( for x in `seq 0 5 $((${#URLS[*]}-1))`; do
   ./htrosbif ${URLS[$x]} -w sigs/${NAMES[$x]}.sig ;
 done; ) &
 
-( for x in `seq 1 3 $((${#URLS[*]}-1))`; do
+( for x in `seq 1 5 $((${#URLS[*]}-1))`; do
   ./htrosbif ${URLS[$x]} -w sigs/${NAMES[$x]}.sig ;
 done; ) &
 
-( for x in `seq 2 3 $((${#URLS[*]}-1))`; do
+( for x in `seq 2 5 $((${#URLS[*]}-1))`; do
+  ./htrosbif ${URLS[$x]} -w sigs/${NAMES[$x]}.sig ;
+done; ) &
+
+( for x in `seq 3 5 $((${#URLS[*]}-1))`; do
+  ./htrosbif ${URLS[$x]} -w sigs/${NAMES[$x]}.sig ;
+done; ) &
+
+( for x in `seq 4 5 $((${#URLS[*]}-1))`; do
   ./htrosbif ${URLS[$x]} -w sigs/${NAMES[$x]}.sig ;
 done; ) &
 
